3c433bdf3ede931ae6280cfcd12a8f4809d9c991
Fivanx10 <77962925+Fivanx10@users.noreply.github.com>
2023-03-24 01:34:17 +0700

    Add files via upload

---
+++ Sait/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+main = Flask(__name__)
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/ticket')
+def ticket():
+    return render_template('ticket.html')
+
+main.run()
